f2741a203da13f5a7a7c793abc50d54f6f1e1b7b
TheoFco <theofranco1@gmail.com>
2026-01-22 15:54:34 -0700

    Update entrypoint.sh to use APR1 hash for htpasswd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,13 @@ set -eu
 : "${BASIC_AUTH_USER:?Set BASIC_AUTH_USER}"
 : "${BASIC_AUTH_PASS:?Set BASIC_AUTH_PASS}"
 
-HASH="$(printf "%s" "$BASIC_AUTH_PASS" | openssl sha1 -binary | openssl base64)"
-printf "%s:{SHA}%s\n" "$BASIC_AUTH_USER" "$HASH" > /tmp/.htpasswd
-
-# readable by nginx worker user
+# Generate an htpasswd-compatible APR1 hash that nginx understands
+HASH="$(openssl passwd -apr1 "$BASIC_AUTH_PASS")"
+printf "%s:%s\n" "$BASIC_AUTH_USER" "$HASH" > /tmp/.htpasswd
 chmod 644 /tmp/.htpasswd
 
+# Start Langflow on an internal port
 langflow run --host 0.0.0.0 --port 7861 &
 
+# Run nginx in the foreground
 exec nginx -g 'daemon off;'
